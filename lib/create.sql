e0637058b1dc8176945b0077175ed8ecd2d6997f
Lia Skalkos <hello@liaskalkos.net>
2015-06-17 13:40:59 -0400

    Complete

---
+++ lib/create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE schools(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  rating INTEGER,
+  year_established INTEGER
+);
+
+CREATE TABLE courses(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  school_id INTEGER
+);
+
+CREATE TABLE students(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE enrollments(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  course_id INTEGER,
+  student_id INTEGER
+);
